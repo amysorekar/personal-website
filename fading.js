b8fd3d3e124e49ab2b00d0132288d19801a59c25
amysorekar <amysorekar@ucsd.edu>
2023-06-28 01:50:00 +0200

    hi

--- fading.js
+++ fading.js
@@ -14,3 +14,17 @@ $(document).ready(function () {
     );
   });
 });
+
+const boxItem = document.querySelectorAll(".main_box li");
+const box = document.querySelectorAll(".box");
+let getSlide = boxItem.length - 1;
+let slideCalc = 30 / getSlide + "%";
+box.forEach((e) => {
+  e.addEventListener("click", function () {
+    box.forEach((box) => {
+      box.style.width = slideCalc;
+      box.classList.remove("active");
+    });
+    e.classList.add("active");
+  });
+});
