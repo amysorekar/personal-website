6fe6b37d6bc798a5e8d9a940093ea7ca3d1b6400
amysorekar <amysorekar@ucsd.edu>
2023-06-29 02:52:41 +0200

    good work

--- script.js
+++ script.js
@@ -12,33 +12,41 @@ $(document).ready(function () {
   });
 });
 
-// accordion menu
-const boxItem = document.querySelectorAll(".main_box li");
-const box = document.querySelectorAll(".box");
-let getSlide = boxItem.length - 1;
-let slideCalc = 0.001 / getSlide + "%";
-box.forEach((e) => {
-  e.addEventListener("click", function () {
-    box.forEach((box) => {
-      box.style.width = slideCalc;
-      box.classList.remove("active");
-    });
-    e.classList.add("active");
-  });
-});
+// // accordion menu
+// const boxItem = document.querySelectorAll(".main_box li");
+// const box = document.querySelectorAll(".box");
+// let getSlide = boxItem.length - 1;
+// let slideCalc = 0.001 / getSlide + "%";
+// box.forEach((e) => {
+//   e.addEventListener("click", function () {
+//     box.forEach((box) => {
+//       box.style.width = slideCalc;
+//       box.classList.remove("active");
+//     });
+//     e.classList.add("active");
+//   });
+// });
 
 // text animation
 const text = document.querySelector(".sec-text");
 const textLoad = () => {
   setTimeout(() => {
-    text.textContent = "Student";
+    text.textContent = "a Student";
   }, 0);
   setTimeout(() => {
-    text.textContent = "Athlete";
+    text.textContent = "an Athlete";
   }, 2000);
   setTimeout(() => {
-    text.textContent = "Scientist";
-  }, 2000); //1s = 1000 milliseconds
+    text.textContent = "a Scientist";
+  }, 4000); //1s = 1000 milliseconds
 };
 textLoad();
-setInterval(textLoad, 12000);
+setInterval(textLoad, 6000);
+
+// toggle menu
+function toggle() {
+  var sec = document.getElementById("sec");
+  var nav = document.getElementById("navigation");
+  sec.classList.toggle("active");
+  nav.classList.toggle("active");
+}
